f9dc3f50d94dfdf0a06c1c898e9223fdcfd58598
Fabrizio Guespe <fguespe@gmail.com>
2026-01-03 03:21:24 -0300

    Update

--- src/lib/index.ts
+++
@@ -1,6 +0,0 @@
-// lib exports - organize utilities here
-export * from "./utils";
-export * from "./xmtp/client";
-export * from "./xmtp/signer";
-export * from "./xmtp/conversations";
-export * from "./agents";
